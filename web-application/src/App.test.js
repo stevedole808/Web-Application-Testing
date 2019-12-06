82a488dfcac76a48ff231cbb839d0afc3f961384
stevedole808 <stevedolelambda@gmail.com>
2019-12-05 23:42:26 -1000

    mvp finished

--- web-application/src/App.test.js
+++ web-application/src/App.test.js
@@ -1,10 +1,11 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 import Dashboard from "./components/Dashboard";
 import Display from "./components/Display";
+import React from 'react';
 
 test("App renders without crashing", () => {
-  render(<App />);
+  render(<App/>);
 });
 
 test("strikes are rendered", () => {
@@ -16,7 +17,6 @@ test("balls are rendered", () => {
   const {queryByText} = render(<Dashboard/>)
   queryByText("balls")
 })
-
 test("Hits are rendered", () => {
   const container = render(<Dashboard/>)
   container.queryByText("/HITS/i")
